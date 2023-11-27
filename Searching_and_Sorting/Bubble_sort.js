16cb2dfd6b56d65504766bde795b48ffdd3e0ec3
utsav kumar <utsavkumar5462@gmail.com>
2023-11-27 21:54:57 +0530

    BubbleSort 2

--- Searching_and_Sorting/Bubble_sort.js
+++ Searching_and_Sorting/Bubble_sort.js
@@ -10,12 +10,22 @@ const takingInput = () => {
   return arr;
 };
 const BubbleSort = (arr) => {
-  for (let i = 0; i <= arr.length; i++) {
-    for (let j = 1; j < arr.length - i; j++) {
-      if (arr[j - 1] > arr[j]) {
-        let temp = arr[j - 1];
-        arr[j - 1] = arr[j];
-        arr[j] = temp;
+  //   for (let i = 0; i < arr.length; i++) {
+  //     console.log(i);
+  //     for (let j = 1; j < arr.length - i; j++) {
+  //       if (arr[j - 1] > arr[j]) {
+  //         let temp = arr[j - 1];
+  //         arr[j - 1] = arr[j];
+  //         arr[j] = temp;
+  //       }
+  //     }
+  //   }
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
